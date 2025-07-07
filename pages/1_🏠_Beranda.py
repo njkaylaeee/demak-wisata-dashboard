228e8fbd0ba5ca84bfa05d19754287ceebd8a009
njkaylaeee <najwazulfa655@students.unnes.ac.id>
2025-07-07 10:57:55 +0700

    Update 1_🏠_Beranda.py

--- pages/1_🏠_Beranda.py
+++ pages/1_🏠_Beranda.py
@@ -1,3 +1,9 @@
 import streamlit as st
+from helper import load_data, plot_data
+
 st.title("🏠 Beranda")
-st.write("Ini adalah halaman awal dashboard prediksi kunjungan wisata.")
+st.write("Halaman ini menampilkan data kunjungan wisata Kabupaten Demak bulan Januari–Juni 2025.")
+
+df = load_data()
+st.pyplot(plot_data(df))
+st.dataframe(df)
