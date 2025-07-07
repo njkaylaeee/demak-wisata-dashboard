ccf8588302586a378add9612bb2e3118b9e949ba
njkaylaeee <najwazulfa655@students.unnes.ac.id>
2025-07-07 11:06:38 +0700

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import folium
-from streamlit_folium import folium_static
 
 def load_data(path="data_wisata.csv"):
     return pd.read_csv(path)
@@ -12,21 +10,3 @@ def plot_data(df):
     plt.xticks(rotation=45)
     plt.tight_layout()
     return plt
-
-def plot_map(df):
-    lokasi_dtw = {
-        'Bedono Bangkit': (-6.8931, 110.6387),
-        'Candisari': (-6.9100, 110.6400),
-        'Bermi - Sumur Gandeng': (-6.9000, 110.6500),
-    }
-    m = folium.Map(location=[-6.9, 110.64], zoom_start=11)
-    total = df.groupby("DTW")[["NUS", "MAN"]].sum().reset_index()
-    for _, row in total.iterrows():
-        if row["DTW"] in lokasi_dtw:
-            popup = f"{row['DTW']}<br>NUS: {int(row['NUS'])}<br>MAN: {int(row['MAN'])}"
-            folium.Marker(
-                location=lokasi_dtw[row["DTW"]],
-                popup=popup,
-                tooltip=row["DTW"]
-            ).add_to(m)
-    folium_static(m, width=900, height=500)
